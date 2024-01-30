0bf5d7be5a4d7ce2af83efebbd39da4567ee8049
DeBolt <daniel.debolt@kochcreativegroup.com>
2024-01-30 13:00:57 -0600

    didnt work
    Signed-off-by: DeBolt <daniel.debolt@kochcreativegroup.com>

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-echo 'building datbase'
-docker pull mcr.microsoft.com/mssql/server:2022-latest
-docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Passw0rd123" -p 1433:1433 --name mssql --hostname local-mssql --platform linux/amd64 -d mcr.microsoft.com/mssql/server:2022-latest
-dotnet kentico-xperience-dbmanager -- -s "localhost" -d "SampleApp" -u "SA" -a "Passw0rd123" -p "Passw0rd123" --license-file "./license.txt" --hash-string-salt "3ead0e85-df41-4ad0-bd64-37c7422eb28e"
-echo 'restoreing database'
-dotnet run --kxp-ci-restore
-echo 'done'
